4fa086e60af576086872677b738ffc93d884fe42
Satyajit Op <73672937+satyajit355@users.noreply.github.com>
2022-06-14 00:17:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client } = require('discord.js-selfbot-v11')
 const client = new Client();
 
 client.on('ready', () => { 
-  client.user.setActivity("varma op", {type: "STREAMING", url: "https://Satyajitop.xyz"})
+  client.user.setActivity("put your status", {type: "STREAMING", url: "https://Satyajitop.xyz"})
    console.log(`${client.user.username} Successfully Logged in!`)
 })
 
