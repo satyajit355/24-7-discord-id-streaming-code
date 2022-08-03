ac0234fe13e7cb0ac7a1bc998db83199726fa81a
Satyajit Op <73672937+satyajit355@users.noreply.github.com>
2022-08-04 02:07:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const keepAlive = require("./server")
-const dotenv = require('dotenv');
-const TOKEN = (process.env.TOKEN);
 const { Client } = require('discord.js-selfbot-v11')
 const client = new Client();
 
@@ -9,5 +6,4 @@ client.on('ready', () => {
    console.log(`${client.user.username} Successfully Logged in!`)
 })
 
-keepAlive()
-client.login(TOKEN);
+client.login("put your token");
